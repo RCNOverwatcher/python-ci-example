e49953b99229d9d8edee9306643af31f97ff7f07
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-05-02 15:21:47 +0100

    done

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/lkhmhb99rssv2mud/branch/main?svg=true)](https://ci.appveyor.com/project/flet-dev/python-ci-example/branch/main)
+![cover](https://user-images.githubusercontent.com/108481836/235510861-86344cc5-da30-403a-befd-13bc69fd38e4.png)
 
-# AppVeyor CI pipeline to build Flet app desktop package
+### JaVelin PA Pi 2023 Challenge entry.
 
-This repository contains a sample "Counter" Python app using Flet framework and CI configuration to package Flet app into standalone desktop bundle with PyInstaller for Windows, macOS and Linux.
+## Instructions
 
-Main features:
+1. Setup the Pi as shown below: <br /><br />
+![diagram](https://user-images.githubusercontent.com/108481836/235369532-3b75fdcf-b0c9-4a12-a9c5-0c8d11d248dd.png)
 
-* Build packages for Windows, macOS and Ubuntu on every commit and upload them into build "Artifacts".
-* Uses Python 3.10 on all thress platforms.
-* Uploads packages to GitHub repository [releases](https://github.com/flet-dev/python-ci-example/releases) when a new tag is pushed.
+2. Enter these three commands in the Raspberry Pi terminal:
 
-To get started with AppVeyor you can just fork this repository and replace `main.py`, `requirements.txt` and `icon.png` with your own.
+    pip install yagmail <br />
+    pip install keyring <br />
+    pip install keyrings.alt <br />
+    pip install flet <br />
+    (If using linux, you may need to install gstreamer): <br />
+    sudo apt-get update <br />
+    sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
+3. Run the python code
+
+## Equipment List
+
+[LDR - Light Dependent Resistor (5 Pack) | The Pi Hut](https://thepihut.com/products/ldr-light-dependent-resistor)
+
+[Premium Male/Male Jumper Wires - 20 x 6" (150mm) | The Pi Hut](https://thepihut.com/products/premium-male-male-jumper-wires-20-x-6-150mm)
+
+[Premium Female/Male 'Extension' Jumper Wires - 20 x 6" | The Pi Hut](https://thepihut.com/products/premium-female-male-extension-jumper-wires-20-x-6)
+
+[Half-Size Breadboard - White | The Pi Hut](https://thepihut.com/products/raspberry-pi-breadboard-half-size)
+
+[Aluminum Electrolytic Capacitor Pack-100 PCS | The Pi Hut](https://thepihut.com/products/aluminum-electrolytic-capacitor-pack-100-pcs)
--- main.py
+++ main.py
@@ -1,31 +1,126 @@
-import flet
-from flet import IconButton, Page, Row, TextField, icons
+from gpiozero import LightSensor
+import time
+import flet as ft
+import yagmail
 
+def main(page: ft.Page):
+    
+    def btnAreaClick(e):
+        ldr = LightSensor(27)
 
-def main(page: Page):
-    page.title = "Flet counter example"
-    page.vertical_alignment = "center"
+        total = 0
 
-    txt_number = TextField(value="0", text_align="right", width=100)
-
-    def minus_click(e):
-        txt_number.value = int(txt_number.value) - 1
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = int(txt_number.value) + 1
-        page.update()
-
-    page.add(
-        Row(
-            [
-                IconButton(icons.REMOVE, on_click=minus_click),
-                txt_number,
-                IconButton(icons.ADD, on_click=plus_click),
-            ],
-            alignment="center",
+        for i in range(1,10):
+            total += (ldr.value * 1000000)
+            lvLDR.controls.append(ft.Text(str(ldr.value)))
+            time.sleep(1)
+            
+        current = 0.0000026455026
+        resistance = 1000000 - (total / 10)
+        power = current * current * resistance
+        powerPerSqMetre = power / 0.00000012
+        
+        area = tbArea.value
+        finalValue = int(area) * powerPerSqMetre
+        finalValuekW = finalValue / 1000
+        moneyGenerated = (finalValuekW * (10/(60*60))) * 16
+        page.add(ft.Text(f"Money made in 10s: {str(moneyGenerated)} pence."))
+        return moneyGenerated
+    
+    def btnEmailClick(e, moneyGenerated):
+            yag = yagmail.SMTP(tbEmail.value, tbPassword.value)
+            yag.send(tbRecipient.value, "SolarX Results", f"You will make {str(moneyGenerated)} pence every 10 seconds!.")
+            page.add(ft.Text("Email sent!"))
+            
+    def themeToggle(e):
+        page.theme_mode = (
+            ft.ThemeMode.DARK
+            if page.theme_mode == ft.ThemeMode.LIGHT
+            else ft.ThemeMode.LIGHT
         )
+        page.update()
+        
+    page.title = "SolarX"
+    page.window_width = 900
+    page.window_height = 700
+    page.window_resizable = False
+    
+    page.appbar = ft.AppBar(
+        title=ft.Text("SolarX"),
+        center_title=True,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED, on_click=themeToggle),
+        ],
     )
+    
+    img = ft.Image(
+        src="assets\cover.png",
+        width=900,
+        fit=ft.ImageFit.CONTAIN,
+    )
+    
+    images = ft.Row(
+        expand=1, 
+        wrap=False, 
+        scroll="always"
+    )
+    
+    tbArea = ft.TextField(
+        label="Area of solar panels (m²): "
+    )
+    
+    btnArea = ft.ElevatedButton(
+        text="Calculate", 
+        on_click=btnAreaClick
+    )
+    
+    tbEmail = ft.TextField(
+        label="Enter your gmail username: "
+    )
+    
+    tbPassword = ft.TextField(
+        label="Enter your password: ", 
+        password=True, 
+        can_reveal_password=True
+    )
+    
+    tbRecipient = ft.TextField(
+        label="Enter the recipient's email address: ",
+        suffix_text="@gmail.com"
+    )
+    
+    btnEmail = ft.ElevatedButton(
+        text="Send Email", 
+        on_click=btnEmailClick
+    )
+    
+    lvLDR = ft.ListView( 
+        spacing=10, 
+        padding=20, 
+        auto_scroll=True
+    )
+    
+    page.add(
+        img, 
+        images,
+        ft.Row(controls=[
+            ft.Column(controls=[
+                tbArea, 
+                btnArea
+            ]),
+            ft.Column(controls=[
+                tbEmail,
+                tbPassword,
+                tbRecipient,
+                btnEmail
+            ]),
+            ft.Column(controls=[
+                lvLDR
+            ])
+        ])
+    )
+    
+    pass
 
-
-flet.app(target=main)
+ft.app(target=main)
